1395b4402233c4a9eabba76bdd3e69c713feb8ac
Artur <arturworkit@gmail.com>
2023-11-15 14:46:49 +0200

    Add createResultString method

--- src/main/java/com/playtech/utils/Utils.java
+++ src/main/java/com/playtech/utils/Utils.java
@@ -3,8 +3,15 @@ package com.playtech.utils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
+
+import com.playtech.models.Player;
 
 public class Utils {
     public static List<String> readData(String fileName) {
@@ -25,5 +32,32 @@ public class Utils {
             System.err.println("Error writing to file: " + fileName);
             e.printStackTrace();
         }
+    }
+
+    public static List<String> createResultString(Map<UUID, Player> players) {
+        List<String> results = new ArrayList<>();
+
+        // List of legitimate players
+        players.values().stream()
+                .filter(player -> player.getOperation().getIsLegal() == true)
+                .sorted(Comparator.comparing(Player::getUuid))
+                .forEach(
+                        player -> results.add(player.getUuid().toString() + " " + player.getBalance().toString() + " " +
+                                String.format(Locale.US, "%.2f", player.calculateWinRate())));
+
+        results.add("");
+
+        // List of illegitimate players
+        players.values().stream()
+                .filter(player -> player.getOperation().getIsLegal() == false)
+                .sorted(Comparator.comparing(Player::getUuid))
+                .forEach(player -> results.add(player.getUuid().toString() + " " + player.getOperation().toString()));
+
+        // Casino balance
+        results.add("");
+        // int casinoBalanceChange = calculateCasinoBalanceChange(players);
+        // results.add(String.valueOf(casinoBalanceChange));
+
+        return results;
     }
 }
