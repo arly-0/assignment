5a7e6d6ec546d1c0ae34d737a35a7871f67d1b1b
Artur <arturworkit@gmail.com>
2023-11-16 00:32:45 +0200

    Refactor main method

--- src/main/java/com/playtech/Main.java
+++ src/main/java/com/playtech/Main.java
@@ -1,31 +1,28 @@
 package com.playtech;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
+import com.playtech.models.Casino;
 import com.playtech.models.Match;
 import com.playtech.models.Player;
+import com.playtech.processors.CasinoProcessor;
 import com.playtech.processors.PlayerProcessor;
 import com.playtech.utils.Utils;
 
 public class Main {
-    public static void main(String[] args) {
-        Long hostBalance = 0l;
-        List<String> playerData = Utils.readData("src/main/resources/player_data.txt");
-        List<String> matchData = Utils.readData("src/main/resources/match_data.txt");
+    public static void main(String[] args) throws IOException {
+        Casino casino = new Casino();
+        List<Player> players = Utils.readPlayers("src/main/resources/player_data.txt");
+        Map<UUID, Match> matches = Utils.readMatches("src/main/resources/match_data.txt");
 
-        Map<UUID, Match> matches = matchData.stream()
-                .map(Match::fromString)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toMap(Match::getUuid, Function.identity()));
+        PlayerProcessor.process(players, matches);
+        CasinoProcessor.process(players, casino);
 
-        Map<UUID, Player> players = PlayerProcessor.processPlayersOperations(playerData, matches);
-        
-        List<String> result = Utils.createResultString(players);
-        Utils.writeResult(result, "src/main/java/com/playtech/result.txt");
+        // List<String> result = Utils.createResultString(players);
+        // Utils.writeResult(result, "src/main/java/com/playtech/result.txt");
+
     }
 }
